0bccb3111ff1a0d3129b6fb8344cf2df38effe75
tongari <info@tongari-as.com>
2017-10-08 12:00:33 +0900

    install faker & crate is seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |idx|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = 'password'
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    provider: '',
+    uid: SecureRandom.uuid,
+    current_sign_in_at: Time.new,
+    last_sign_in_at: Time.new,
+    confirmed_at: Time.new,
+    confirmation_sent_at: Time.new
+  )
+
+  Topic.create!(
+    photo: '',
+    comment: "#{Faker::DragonBall.character} is great!",
+    user_id: rand(10) + 1
+  )
+end
